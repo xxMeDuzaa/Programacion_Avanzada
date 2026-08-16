c6827979679af6fc6ae3093b7278b21c6f866a6e
xxMeDuzaa <mariamelinaduarte@gmail.com>
2026-08-16 01:16:40 -0300

    Primeros 4 puntos del primer apartado 01

--- TP2/Punto1.js
+++ TP2/Punto1.js
@@ -3,14 +3,52 @@
 //                                  //
 
 // PUNTO 1
-let libro = {
+console.log ("----------------- Punto 1 ---------------------")
+const libro = {
     titulo: "El Principito",
     autor: "Antoine de Saint-Exupéry",
     anio: 1943,
-   }
+}
 
-console.log(libro.titulo); 
-console.log(libro.autor); 
-console.log(libro.anio); 
+console.log("Titulo: ", libro.titulo); 
+console.log("Autor:  ", libro.autor); 
+console.log("Anio: ", libro.anio); 
+
 
 //PUNTO 2
+console.log ("----------------- Punto 2 ---------------------")
+const estudiante = {
+    nombre_completo: "Juan Pérez",
+    edad: 20,
+    direccion: {
+        calle: "Calle Falsa 123",
+        ciudad: "Usuahia",
+        pais: "Argentina",
+    }
+};
+
+console.log ("Direccion del estudiante: ", estudiante.nombre_completo)
+console.log (
+  `Dirección: ${estudiante.direccion.calle}, ${estudiante.direccion.ciudad}, ${estudiante.direccion.pais}`
+);
+
+
+//PUNTO 3
+console.log ("----------------- Punto 3 ---------------------")
+libro.descripción = function() {
+  return `"${this.titulo}" fue escrito por ${this.autor}.`;
+};
+
+console.log(libro.descripción());
+
+//PUNTO 4
+console.log ("----------------- Punto 4 ---------------------")
+const producto = {
+    nombre: "Kindel",
+    precio: 500,
+    disponible: true,
+}
+
+for (let propiedad in producto) {
+  console.log(`${propiedad}: ${producto[propiedad]}`);
+}
