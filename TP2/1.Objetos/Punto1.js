b06785fc7dd31502cc7c4240ba20275dbe7cb4a3
xxMeDuzaa <mariamelinaduarte@gmail.com>
2026-08-16 00:29:16 -0300

    Correccion de acceso a los archivos

--- TP2/1.Objetos/Punto1.js
+++
@@ -1,9 +0,0 @@
-let libro = {
-    titulo: "El Principito",
-    autor: "Antoine de Saint-Exupéry",
-    anio: 1943,
-   }
-
-console.log(libro.titulo); 
-console.log(libro.autor); 
-console.log(libro.anio); 
---
+++ TP2/Punto1.js
@@ -0,0 +1,16 @@
+//                                  //
+//   01: Ejercicios sobre Objetos   //
+//                                  //
+
+// PUNTO 1
+let libro = {
+    titulo: "El Principito",
+    autor: "Antoine de Saint-Exupéry",
+    anio: 1943,
+   }
+
+console.log(libro.titulo); 
+console.log(libro.autor); 
+console.log(libro.anio); 
+
+//PUNTO 2
