916879fc696d7e10c9bbcc0d15cf1293840e9d0d
xxMeDuzaa <mariamelinaduarte@gmail.com>
2026-08-17 14:20:10 -0300

    Primera parte completa

--- TP2/Punto1.js
+++ TP2/Punto1.js
@@ -3,7 +3,7 @@
 //                                  //
 
 // PUNTO 1
-console.log ("----------------- Punto 1 ---------------------")
+console.log ("----------------- Punto 1 ---------------------");
 const libro = {
     titulo: "El Principito",
     autor: "Antoine de Saint-Exupéry",
@@ -16,33 +16,34 @@ console.log("Anio: ", libro.anio);
 
 
 //PUNTO 2
-console.log ("----------------- Punto 2 ---------------------")
+console.log ("----------------- Punto 2 ---------------------");
 const estudiante = {
-    nombre_completo: "Juan Pérez",
+    nombre: "Juan Pérez",
     edad: 20,
     direccion: {
-        calle: "Calle Falsa 123",
+        calle: "Maipu 5",
         ciudad: "Usuahia",
         pais: "Argentina",
     }
 };
 
-console.log ("Direccion del estudiante: ", estudiante.nombre_completo)
+console.log ("Direccion del estudiante: ", estudiante.nombre);
 console.log (
   `Dirección: ${estudiante.direccion.calle}, ${estudiante.direccion.ciudad}, ${estudiante.direccion.pais}`
 );
 
 
 //PUNTO 3
-console.log ("----------------- Punto 3 ---------------------")
+console.log ("----------------- Punto 3 ---------------------");
 libro.descripción = function() {
   return `"${this.titulo}" fue escrito por ${this.autor}.`;
 };
 
 console.log(libro.descripción());
 
+
 //PUNTO 4
-console.log ("----------------- Punto 4 ---------------------")
+console.log ("----------------- Punto 4 ---------------------");
 const producto = {
     nombre: "Kindel",
     precio: 500,
@@ -52,3 +53,75 @@ const producto = {
 for (let propiedad in producto) {
   console.log(`${propiedad}: ${producto[propiedad]}`);
 }
+
+
+//PUNTO 5
+console.log ("----------------- Punto 5 ---------------------");
+producto.precio = 1350;
+
+console.log("Producto Actualizado: ", producto);
+
+
+//PUNTO 6
+console.log ("----------------- Punto 6 ---------------------");
+function tienePropiedad(objeto, propiedad) {
+  return propiedad in objeto;
+}
+
+console.log("¿Tiene la propiedad 'precio'?:", tienePropiedad(producto, "precio"));
+console.log("¿Tiene la propiedad 'descuento'?:", tienePropiedad(producto, "descuento"));
+
+
+//PUNTO 7
+console.log ("----------------- Punto 7 ---------------------");
+console.log("Antes de eliminar 'disponible': ", producto);
+delete producto.disponible;
+console.log("Despues de eliminar 'disponible': ", producto);
+
+
+//PUNTO 8
+console.log ("----------------- Punto 8 ---------------------");
+const persona1 = { nombre: "Carlos", edad: 30 };
+const persona2 = { profesion: "Ingeniero", ciudad: "Mendoza" };
+
+console.log("Objeto no combinado:", persona1);
+console.log("Objeto no combinado:", persona2);
+
+const personaCombinada = Object.assign({}, persona1, persona2);
+
+console.log("Objeto combinado:", personaCombinada);
+
+
+//PUNTO 9
+console.log ("----------------- Punto 9 ---------------------");
+const estudianteCopia = JSON.parse(JSON.stringify(estudiante));
+
+// Modificacion de la copia
+estudianteCopia.nombre = "Sofia";
+estudianteCopia.direccion.ciudad = "Córdoba";
+
+console.log("Copia modificada:", estudianteCopia.nombre, "-", estudianteCopia.direccion.ciudad);
+console.log("Original sin alteración:", estudiante.nombre, "-", estudiante.direccion.ciudad);
+
+
+//PUNTO 10
+console.log ("----------------- Punto 10 ---------------------");
+Object.defineProperty(libro, "añoPublicacion", {   //definicion de reglas de acceso al objeto
+  get() {
+    return this.añoDePublicacion;
+  },
+  set(nuevoAño) {
+    if (typeof nuevoAño === "number" && nuevoAño > 0) {
+      this.añoDePublicacion = nuevoAño;
+    } else {
+      console.log("Año no válido");
+    }
+  }
+});
+
+console.log("Año de publicación actual:", libro.anio); //antes de modificar
+
+libro.añoPublicacion = 1970; //le asigna la modificacion del  dato
+
+// Uso del getter
+console.log("Año de publicación actualizado vía Getter:", libro.añoPublicacion); //printea la modificacion
